b2e1bb681971ae555e16fba43386f34d6688fc7e
Humaira Rahaman <humairamitul@gmail.com>
2022-02-15 02:20:41 +0600

    pin matching

--- pin-matcher/app.js
+++ pin-matcher/app.js
@@ -15,3 +15,39 @@ function genaratePin(){
    const pin = getPin();
    document.getElementById("show-pin").value = pin;
 }
+
+//calculation part
+document.getElementById("key-pad").addEventListener("click", function(event){
+    const number = event.target.innerText;
+    const calcInput = document.getElementById("typed-numbers");
+    if(isNaN(number)){
+        if(number == 'C'){
+            calcInput.value = '';
+        }
+    }
+    else{
+    const prevNumber = calcInput.value;
+    const newNumber = prevNumber + number;
+    calcInput.value = newNumber;
+    }
+   
+    
+})
+// maching pin number
+
+function verifyPin(){
+    const pin = document.getElementById('show-pin').value;
+    const typeNumbers = document.getElementById("typed-numbers").value;
+    const notifySuccess= document.getElementById("notify-success");
+    const notifyFail= document.getElementById("notify-fail");
+    if(pin == typeNumbers){
+        
+        notifySuccess.style.display = 'block';
+        notifyFail.style.display = 'none';
+    }
+    else{
+        
+        notifyFail.style.display = 'block';
+        notifySuccess.style.display = 'none';
+    }
+}
